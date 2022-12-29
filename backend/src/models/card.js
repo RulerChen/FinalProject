b4aa402e225abcfef395a78787457e74157fe696
RulerChen <a20196780@gmail.com>
2022-12-29 19:37:24 +0800

    UPDATE: deploy

--- backend/src/models/card.js
+++ backend/src/models/card.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 /******* User Schema *******/
 const cardSchema = new Schema({
   id: { type: String, required: [true, "from is required."] },
-  url: { type: String},
-  text: { type: String},
-  tag: {type: String},
+  url: { type: String },
+  text: { type: String },
+  tag: { type: String },
 });
 const CardModel = mongoose.model("Card", cardSchema);
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,27 @@ import dotenv from "dotenv-defaults";
 import path from "path";
 //graphql
 import { createServer } from "http";
-import { CardModel  } from "./models/card.js";
+import { CardModel } from "./models/card.js";
 import * as fs from "fs";
 import { createSchema, createYoga } from "graphql-yoga";
 import Query from "./resolvers/Query.js";
 import Mutation from "./resolvers/Mutation.js";
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") app.use(cors());
+db.connect();
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
 const yoga = createYoga({
   schema: createSchema({
     typeDefs: fs.readFileSync("./src/schema.graphql", "utf-8"),
@@ -23,26 +39,5 @@ const yoga = createYoga({
   },
 });
 //http://finalproject-u4ak.onrender.com/
-db.connect();
 const server = createServer(yoga);
 server.listen(5000, () => console.log(`listening on port http://localhost:${5000}/graphql!`));
-//*下面沒動*
-
-dotenv.config();
-
-const app = express();
-
-if (process.env.NODE_ENV === "development") app.use(cors());
-
-db.connect();
-
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-  });
-}
-
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
