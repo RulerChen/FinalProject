ebfe3878f447ab73857913a0967f81982b1639fd
b10705044 <96435864+b10705044@users.noreply.github.com>
2022-12-30 21:38:27 +0800

    deleteGraphQL

--- backend/src/resolvers/Mutation.js
+++
@@ -1,7 +0,0 @@
-const Mutation = {
-  createCard: async (parent, { id, url, text, tag }, { CardModel }) => {
-    const newData = await new CardModel({ id, url, text, tag });
-    return newData;
-  },
-};
-export default Mutation;
--- backend/src/resolvers/Query.js
+++
@@ -1,7 +0,0 @@
-const Query = {
-  card: async (parent, { id }, { CardModel }) => {
-    let data = await CardModel.find({ id });
-    return data;
-  },
-};
-export default Query;
